a5880f25a8845855a99bb0b5af5154ba9b9b4d6b
SidorovOS <OSeSidorov@sberbank.ru>
2024-08-04 21:11:41 +0300

    Добавлен линтер по проверки импорта из паблик айпи

--- tests/lib/rules/public-api-impots.js
+++ tests/lib/rules/public-api-impots.js
@@ -2,30 +2,48 @@
  * @fileoverview f
  * @author villimon
  */
-"use strict";
+'use strict'
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/public-api-impots"),
-  RuleTester = require("eslint").RuleTester;
-
+const rule = require('../../../lib/rules/public-api-impots'),
+    RuleTester = require('eslint').RuleTester
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
-ruleTester.run("public-api-impots", rule, {
-  valid: [
-    // give me some code that won't trigger a warning
-  ],
-
-  invalid: [
+const aliasOptions = [
     {
-      code: "",
-      errors: [{ messageId: "Fill me in.", type: "Me too" }],
+        alias: '@',
     },
-  ],
-});
+]
+
+const ruleTester = new RuleTester()
+ruleTester.run('public-api-impots', rule, {
+    valid: [
+        {
+            code: "import {addCommentFormActions,addCommentFormReducer} from '../../model/slice/addCommentFormSlice';",
+            errors: [],
+        },
+        {
+            code: "import {addCommentFormActions,addCommentFormReducer} from '@/pages/Articles'",
+            errors: [],
+            options: aliasOptions,
+        },
+    ],
+
+    invalid: [
+        {
+            code: "import {addCommentFormActions,addCommentFormReducer} from '@/pages/Article/model/slice/addCommentFormSlice';",
+            errors: [
+                {
+                    message: 'Абсолютный импорт разрешен только из Publick API',
+                },
+            ],
+            options: aliasOptions,
+        },
+    ],
+})
