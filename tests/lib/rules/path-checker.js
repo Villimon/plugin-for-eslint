6e671ade4156f43c0e78b23de0240d85cf7afa53
Villimon <sidorov.oleg98@mail.ru>
2024-08-04 13:30:17 +0300

    first commit

---
+++ tests/lib/rules/path-checker.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview feature sliced relative path checker
+ * @author villimon
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/path-checker'),
+    RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run('path-checker', rule, {
+    valid: [
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: '',
+            errors: [{ messageId: 'Fill me in.', type: 'Me too' }],
+        },
+    ],
+});
